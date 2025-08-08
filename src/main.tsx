c03fe61140e329f8ba5856dcd742a9def22b7b74
Olly Rowe <oliver.rowe101@gmail.com>
2025-08-08 18:50:45 +0200

    fix: update router base name

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename={import.meta.env.VITE_BASE_PATH || '/'}>
+  <BrowserRouter basename="/shopping-list/">
     <App />
   </BrowserRouter>
 );
