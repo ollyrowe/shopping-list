93ca74a969fd12b67bc859c682caa5478571f498
Olly Rowe <oliver.rowe101@gmail.com>
2025-08-19 17:55:16 +0100

    feat: add basic offline support

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
+import './sw';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter basename="/shopping-list/">
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,64 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
+import { VitePWA } from 'vite-plugin-pwa';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   base: '/shopping-list/',
   plugins: [
     react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png', 'assets/*'],
+      manifest: {
+        name: 'Shopping List',
+        short_name: 'Shopping List',
+        description: 'A shopping list app',
+        theme_color: '#ffffff',
+        start_url: '/shopping-list/',
+        icons: [
+          {
+            src: '/shopping-list/favicon.ico',
+            sizes: '48x48',
+            type: 'image/x-icon',
+          },
+          {
+            src: '/shopping-list/web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/shopping-list/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        // Serve the index.html for all navigation requests
+        navigateFallback: '/shopping-list/index.html',
+        runtimeCaching: [
+          {
+            urlPattern: /^\/shopping-list\/.*$/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'html-cache',
+            },
+          },
+          {
+            urlPattern: ({ request }) =>
+              request.destination === 'script' ||
+              request.destination === 'style' ||
+              request.destination === 'worker',
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'asset-cache',
+            },
+          },
+        ],
+      },
+    }),
     tsconfigPaths(),
     checker({
       typescript: { tsconfigPath: './tsconfig.app.json' },
