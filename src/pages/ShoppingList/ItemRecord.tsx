d66b42db73939a97b8ab1a6d8447e62d0b9d718a
Olly Rowe <oliver.rowe101@gmail.com>
2025-09-14 18:49:06 +0100

    refactor: move list item record to new file

---
+++ src/pages/ShoppingList/ItemRecord.tsx
@@ -0,0 +1,114 @@
+import { motion, type TargetAndTransition } from 'framer-motion';
+import {
+  ActionIcon,
+  Box,
+  Checkbox,
+  Divider,
+  Flex,
+  Text,
+  useComputedColorScheme,
+} from '@mantine/core';
+import { uncategorisedItemIcon } from '@/services';
+import type { Item } from '@/types';
+
+interface ItemRecordProps {
+  item: Item;
+  isBeingCleared?: boolean;
+  visible?: boolean;
+  onClick?: () => void;
+  onCheck?: () => void;
+  onChangeCategory?: () => void;
+}
+
+const ItemRecord: React.FC<ItemRecordProps> = ({
+  item,
+  isBeingCleared = false,
+  visible = true,
+  onClick,
+  onCheck,
+  onChangeCategory,
+}) => {
+  const handleChangeCategory = (e: React.MouseEvent) => {
+    e.stopPropagation();
+
+    if (onChangeCategory) {
+      onChangeCategory();
+    }
+  };
+
+  const colorScheme = useComputedColorScheme();
+
+  const checkedItemColor =
+    colorScheme === 'light' ? 'var(--mantine-color-gray-0)' : 'var(--mantine-color-dark-8)';
+
+  return (
+    <motion.div
+      initial={itemInitialAnimation}
+      animate={isBeingCleared ? itemExitAnimation : itemEnterAnimation}
+      exit={visible ? itemExitAnimation : undefined}
+      layout
+    >
+      <Box>
+        <Flex
+          px="md"
+          py="sm"
+          align="center"
+          bg={item.checked ? checkedItemColor : 'var(--mantine-color-body)'}
+          onClick={onClick}
+        >
+          <Checkbox
+            radius="lg"
+            mr="md"
+            color="green"
+            checked={!!item.checked}
+            onChange={onCheck}
+            onClick={(e) => e.stopPropagation()}
+          />
+          <Text c="gray" fw="bold" tt="lowercase" mr="auto">
+            {item.name}
+          </Text>
+          {item.quantity > 1 && (
+            <Text c="gray" size="sm" mr="md">
+              {item.quantity}
+              {item.unit}
+            </Text>
+          )}
+          <ActionIcon variant="subtle" radius="xl" color="gray" onClick={handleChangeCategory}>
+            {item.category ? item.category.icon : uncategorisedItemIcon}
+          </ActionIcon>
+        </Flex>
+        <Divider />
+      </Box>
+    </motion.div>
+  );
+};
+
+export default ItemRecord;
+
+export const itemAnimationDurationSeconds = 0.3;
+
+const itemInitialAnimation: TargetAndTransition = {
+  opacity: 1,
+  scale: 1,
+  x: -100,
+};
+
+const itemExitAnimation: TargetAndTransition = {
+  opacity: 0,
+  scale: 0.8,
+  x: -100,
+  transition: {
+    duration: itemAnimationDurationSeconds,
+    ease: 'easeInOut',
+  },
+};
+
+const itemEnterAnimation: TargetAndTransition = {
+  opacity: 1,
+  scale: 1,
+  x: 0,
+  transition: {
+    duration: itemAnimationDurationSeconds,
+    ease: 'easeInOut',
+  },
+};
--- src/pages/ShoppingList/ShoppingListPage.tsx
+++ src/pages/ShoppingList/ShoppingListPage.tsx
@@ -7,12 +7,11 @@ import {
   IconSearch,
   IconSortAscendingShapes,
 } from '@tabler/icons-react';
-import { AnimatePresence, motion, type TargetAndTransition } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import {
   ActionIcon,
   Box,
   Button,
-  Checkbox,
   Divider,
   Flex,
   Progress,
@@ -26,8 +25,8 @@ import ChangeCategoryModal from '@/components/ChangeCategoryModal';
 import ItemDrawer from '@/components/ItemDrawer';
 import SortItemsModal from '@/components/SortListModal';
 import { useShoppingList } from '@/providers/ShoppingListProvider';
-import { uncategorisedItemIcon } from '@/services';
 import { type Category, type Item } from '@/types';
+import ItemRecord, { itemAnimationDurationSeconds } from './ItemRecord';
 
 const ShoppingListPage: React.FC = () => {
   const { shoppingList, categories, updateItem } = useShoppingList();
@@ -281,103 +280,3 @@ const ShoppingListPage: React.FC = () => {
 };
 
 export default ShoppingListPage;
-
-interface ItemRecordProps {
-  item: Item;
-  isBeingCleared?: boolean;
-  visible?: boolean;
-  onClick?: () => void;
-  onCheck?: () => void;
-  onChangeCategory?: () => void;
-}
-
-const ItemRecord: React.FC<ItemRecordProps> = ({
-  item,
-  isBeingCleared = false,
-  visible = true,
-  onClick,
-  onCheck,
-  onChangeCategory,
-}) => {
-  const handleChangeCategory = (e: React.MouseEvent) => {
-    e.stopPropagation();
-
-    if (onChangeCategory) {
-      onChangeCategory();
-    }
-  };
-
-  const colorScheme = useComputedColorScheme();
-
-  const checkedItemColor =
-    colorScheme === 'light' ? 'var(--mantine-color-gray-0)' : 'var(--mantine-color-dark-8)';
-
-  return (
-    <motion.div
-      initial={itemInitialAnimation}
-      animate={isBeingCleared ? itemExitAnimation : itemEnterAnimation}
-      exit={visible ? itemExitAnimation : undefined}
-      layout
-    >
-      <Box>
-        <Flex
-          px="md"
-          py="sm"
-          align="center"
-          bg={item.checked ? checkedItemColor : 'var(--mantine-color-body)'}
-          onClick={onClick}
-        >
-          <Checkbox
-            radius="lg"
-            mr="md"
-            color="green"
-            checked={!!item.checked}
-            onChange={onCheck}
-            onClick={(e) => e.stopPropagation()}
-          />
-          <Text c="gray" fw="bold" tt="lowercase" mr="auto">
-            {item.name}
-          </Text>
-          {item.quantity > 1 && (
-            <Text c="gray" size="sm" mr="md">
-              {item.quantity}
-              {item.unit}
-            </Text>
-          )}
-          <ActionIcon variant="subtle" radius="xl" color="gray" onClick={handleChangeCategory}>
-            {item.category ? item.category.icon : uncategorisedItemIcon}
-          </ActionIcon>
-        </Flex>
-        <Divider />
-      </Box>
-    </motion.div>
-  );
-};
-
-const itemAnimationDurationSeconds = 0.3;
-
-const itemInitialAnimation: TargetAndTransition = {
-  opacity: 1,
-  scale: 1,
-  x: -100,
-};
-
-const itemExitAnimation: TargetAndTransition = {
-  opacity: 0,
-  scale: 0.8,
-  x: -100,
-  transition: {
-    duration: itemAnimationDurationSeconds,
-    ease: 'easeInOut',
-  },
-};
-
-const itemEnterAnimation: TargetAndTransition = {
-  opacity: 1,
-  scale: 1,
-  x: 0,
-  transition: {
-    duration: itemAnimationDurationSeconds,
-    ease: 'easeInOut',
-  },
-};
